41d6bebb8f50ce94ae787da7a020a773dd298b86
Zero Cho <itszero@gmail.com>
2021-12-10 21:06:27 -1000

    dynamic lookup

--- src/advent_2021/core.clj
+++ src/advent_2021/core.clj
@@ -1,29 +1,20 @@
 (ns advent-2021.core
   (:gen-class))
 
-(require 'advent-2021.day1)
-(require 'advent-2021.day2)
-(require 'advent-2021.day3)
-(require 'advent-2021.day4)
-(require 'advent-2021.day5)
-(require 'advent-2021.day6)
-(require 'advent-2021.day7)
-(require 'advent-2021.day8)
-(require 'advent-2021.day9)
-(require 'advent-2021.day10)
-(require 'clojure.java.io)
+(require '[advent-2021.utils :refer [debug]])
+(require '[clojure.string :as str])
+(require '[clojure.java.io :as jio])
+
+(def days (->> (jio/file "./src/advent_2021")
+               (file-seq)
+               (filter #(.isFile %))
+               (filter #(str/includes? (.getName %) "day"))
+               (map #(.getName %))
+               (map (fn [file-name] (str/replace file-name #"day(\d+).clj" #(str "day" (%1 1)))))))
+
+(doall (map #(require (symbol (str "advent-2021." %))) days))
 
 (def -main
   (fn [& args]
-    (with-open [file (clojure.java.io/reader (second args))]
-      ((case (first args)
-         "day1" advent-2021.day1/main
-         "day2" advent-2021.day2/main
-         "day3" advent-2021.day3/main
-         "day4" advent-2021.day4/main
-         "day5" advent-2021.day5/main
-         "day6" advent-2021.day6/main
-         "day7" advent-2021.day7/main
-         "day8" advent-2021.day8/main
-         "day9" advent-2021.day9/main
-         "day10" advent-2021.day10/main) file))))
+    (with-open [file (jio/reader (second args))]
+      ((ns-resolve (symbol (str "advent-2021." (first args))) (symbol "main")) file))))
